ecba9f85666df512b0f660a2c25f05eb721cc3f4
John Bolduc <johnboldu@gmail.com>
2017-08-13 18:34:00 -0400

    create item model controller views

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170811162643) do
+ActiveRecord::Schema.define(version: 20170811192411) do
+
+  create_table "items", force: :cascade do |t|
+    t.string "categorie"
+    t.string "listing_name"
+    t.text "summary"
+    t.string "address"
+    t.integer "price"
+    t.boolean "active"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
